89c814141f3937c7548661b2e505715965162e91
hunter <westerhr@mail.uc.edu>
2024-12-12 12:57:47 -0500

    Final commit

--- src/Reggie.java
+++ src/Reggie.java
@@ -2,6 +2,14 @@ import java.util.Scanner;
 
 public class Reggie {
     public static void main(String[] args) {
-        
+        Scanner scan = new Scanner(System.in);
+        String ssn = SafeInput.getRegExString(scan, "Enter a valid ssn (###-##-####)", "^\\d{3}-\\d{2}-\\d{4}$");
+        System.out.println("Valid SSN entered: " + ssn);
+
+        String mNumber = SafeInput.getRegExString(scan, "Enter a valid UC M Number:", "^(M|m)\\d{5}$");
+        System.out.println("UC M Number entered: " + mNumber);
+
+        String menuChoice = SafeInput.getRegExString(scan, "Enter a menu choice (O/o) for Open, (V/v) for View, (S/s) for Save, and (Q/q) for quit", "^[OoSsVvQq]$");
+        System.out.println("Menu choice entered: " + menuChoice);
     }
 }
